bb326a76daa53c1be64a02d3a7c940f4286be286
Mohamad Ahmadi <Moein@Mohamads-MacBook-Air.local>
2023-03-31 04:46:55 +0330

    add new features

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "eaee86654a872b524cb27da22987b16d",
-"index.html": "6b9ef272abce37c6258dc224131b8074",
-"/": "6b9ef272abce37c6258dc224131b8074",
-"main.dart.js": "11e5bc5b2a8eaa44ffd1826ad09241ba",
+"index.html": "779d4c0c7d96aa4b5601fc2d0721afc3",
+"/": "779d4c0c7d96aa4b5601fc2d0721afc3",
+"main.dart.js": "b71db8301894bcf298bff59f0320c23c",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
